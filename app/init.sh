e093cfedbc9d49cd0168e5905bbb7ab97403aad8
Loïc <loic@jeedom.com>
2015-12-29 11:07:12 +0100

    Some cleaning

--- app/init.sh
+++ app/init.sh
@@ -8,22 +8,22 @@ else
 	echo "root:${ROOT_PASSWORD}" | chpasswd
 fi
 
-if [ -d /var/www/jeedom/core ]; then
+if [ -d /var/www/core ]; then
 	echo 'Jeedom is already install'
 else
-	mkdir -p /var/www/jeedom
+	mkdir -p /var/www
 	echo 'Jeedom not found install it'
 	rm -rf /root/core-*
 	wget https://github.com/jeedom/core/archive/beta.zip -O /tmp/jeedom.zip
 	unzip -q /tmp/jeedom.zip -d /root/
-	cp -R /root/core-*/* /var/www/jeedom/
-	cp -R /root/core-*/.htaccess /var/www/jeedom/
+	cp -R /root/core-*/* /var/www/
+	cp -R /root/core-*/.htaccess /var/www/
 fi
 
 echo 'All init complete'
 chmod 777 /dev/tty*
-chmod 755 -R /var/www/jeedom/
-chown -R www-data:www-data /var/www/jeedom/
+chmod 755 -R /var/www
+chown -R www-data:www-data /var/www
 
 echo 'Launch cron'
 service cron start
